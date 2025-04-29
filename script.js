ef5a0b5189126273d2b85be47c16aa4d950ff0d6
Olzhas <olzhas228335@gmail.com>
2025-04-29 20:08:56 +0600

    audio dobav

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+
 const cards = [
-    { front: '<img src = "src/france.jpg">', back: 'France — Paris' },
-    { front: '<img src = "src/jpn.png">', back: 'Japan — Tokyo' },``
+    { front: '<img src = "src/france.jpg">', back: 'France ' },
+    { front: '<img src = "src/jpn.png">', back: 'Japan ' },
+    { front: '<img src = "src/italy.png">', back: 'Italy ' },
+    { front: '<img src = "src/uk.png">', back: 'UK' },
+    { front: '<img src = "src/">', back: 'Japan — Tokyo' },
+    { front: '<img src = "src/">', back: 'Japan — Tokyo' },
+    { front: '<img src = "src/">', back: 'Japan — Tokyo' },
   ];
   let currentIndex = 0;
   
   function flipCard() {
     document.getElementById('card').classList.toggle('flip');
+    flipBtn.addEventListener('click', () => {
+      cardEl.classList.toggle('flipped');
+      // проигрываем звук
+      flipSound.currentTime = 0;  // сбросить на начало
+      flipSound.play('src/Audio.mp3');
+    });
+    
   }
   
   function nextCard() {
@@ -14,6 +27,17 @@ const cards = [
     document.getElementById('back').innerHTML = cards[currentIndex].back;
     document.getElementById('card').classList.remove('flip');
   }
+
+
+
+  function previousCard() {
+    currentIndex = (currentIndex - 1) % cards.length;
+    document.getElementById('back').innerHTML = cards[currentIndex].back;
+    document.getElementById('front').innerHTML = cards[currentIndex].front;
+    document.getElementById('card').classList.remove('flip');
+  }
+
+
 
 
 
